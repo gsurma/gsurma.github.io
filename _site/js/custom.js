dc9e06949cf21372323df705e0fe45cff452cd82
Grzegorz Surma <grzegorz.surma@icloud.com>
2018-04-16 16:53:07 +0200

    refactor

---
+++ _site/js/custom.js
@@ -0,0 +1,27 @@
+ $(window).load(function(){
+        var $container = $('.portfolioContainer');
+        $container.isotope({
+            filter: '*',
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+        });
+
+        $('.portfolioFilter a').click(function(){
+            $('.portfolioFilter .current').removeClass('current');
+            $(this).addClass('current');
+
+            var selector = $(this).attr('data-filter');
+            $container.isotope({
+                filter: selector,
+                animationOptions: {
+                    duration: 750,
+                    easing: 'linear',
+                    queue: false
+                }
+             });
+             return false;
+        });
+    });
